577ac7b53bde9f81f4e121860660a103343d0ead
Kleber <limadasilvakleber@gmal.com>
2024-11-22 20:53:11 -0300

    code optmized

--- exDesafioMedia.js
+++ exDesafioMedia.js
@@ -5,16 +5,16 @@ let Nota1 = parseFloat(prompt("Qual a primeira nota do aluno?"));
 let Nota2 = parseFloat(prompt("Qual a segunda nota do aluno?"));
 let Nota3 = parseFloat(prompt("Qual a terceira nota do aluno?"));
 let Falta = Number(prompt("Qual a porcentagem de falta do aluno?"));
-let media = parseInt((Nota1+Nota2+Nota3)/3)
+let media = parseInt((Nota1+Nota2+Nota3)/3).toFixed(2)
 //Teste logico se o aluno tiver media maior ou igual a 7 e menos de 25% de falta ele esta APROVADO
-if(media>=7 && Falta<25) {
-    console.log("Aluno aprovado");
+if(media<5 || Falta>=25) {
+    console.log("O aluno esta reprovado");
 }
 //Teste logico se o aluno tiver media menor que 7 porém maior ou igual a 5 e menos de 25% de falta ele esta de RECUPERAÇÃO
-else if(media<7 && media>=5 && Falta<25) {
+else if(media<7 && media>=5) {
     console.log("O aluno esta de recuperação");
 } 
 //Teste logico se o aluno tiver media menor que 5 e mais de/ou 25% de falta ele esta de REPROVADO
-else if(media<5 && Falta>=25){
-    console.log("O aluno esta reprovado");
+else{
+    console.log("Aluno aprovado");
 };
